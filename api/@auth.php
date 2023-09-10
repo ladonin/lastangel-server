c0ac3e538d3541c8272839fc723a2affa172d96e
aladonin <ladonin85@mail.ru>
2023-09-10 05:16:06 +0300

    #LA-11

--- api/@auth.php
+++ api/@auth.php
@@ -26,7 +26,7 @@ function auth_createToken($data) {
  * Проверяется токен и сравнивается полученная роль с допустимой
  */
 function auth_verify($roles) {
-	global $AUTH_SECRET_KEY;	
+	global $AUTH_SECRET_KEY;
 	$_headers = apache_request_headers();
 
 	$_token = $_headers['Authorization'];
@@ -41,5 +41,21 @@ function auth_verify($roles) {
 	functions_errorOutput('Данному пользователю доступ запрещен.', 403);
 }
 
+/*
+ * Определение роли
+ */
+function auth_get_role() {
+	global $AUTH_SECRET_KEY;
+	$_headers = apache_request_headers();
+	if (!isset($_headers['Authorization'])) return '';
+	$_token = $_headers['Authorization'];
 
+	try {
+		$_decoded = JWT::decode($_token, new Key($AUTH_SECRET_KEY, 'HS256'));
+	} catch (Exception $e) {
+		// echo($e->getMessage());
+		functions_errorOutput('Ошибка авторизации.', 403);
+	}
+	return $_decoded->r;
+}
 ?>
